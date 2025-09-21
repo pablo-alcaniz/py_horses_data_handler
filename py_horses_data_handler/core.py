a1faef86ef4e6e7d5cb03b1390ed625f21de52c5
alca <alcaniz.pablo@gmail.com>
2025-09-21 22:45:06 +0200

    dir checks and management

---
+++ py_horses_data_handler/core.py
@@ -0,0 +1,39 @@
+import os
+import glob
+
+
+class HorsesDataHandler():
+    
+    def __init__(self):
+        print(">>> Data handler started...")
+    
+    def convert_hsol2hdf(self, SOLVER_PATH:str, SOL_PATH: str, MESH_PATH: str):
+        ########################################################################
+        ##  Function to convert .hsol binaries to .hdf
+        ########################################################################
+        
+        
+        ## DIR checks and manipilation:
+        
+        # horses2plt:
+        horses2plt_RELATIVE_DIR = "Solver/bin/horses2plt"
+        horses2plt_ABS_DIR = os.path.join(SOLVER_PATH,horses2plt_RELATIVE_DIR)
+        
+        if not os.path.isfile(horses2plt_ABS_DIR): #check if horses2plt exists in the directory provided by user
+            raise Exception("The horses2plt utility is not found.")
+
+        # solution files:
+        hsol_files = [file for file in os.listdir(SOL_PATH) if file.endswith(f".hsol")] #makes a list of hsol files
+        
+        if not hsol_files: 
+            raise Exception("No .hsol found.")
+        else:
+            print(">>> Nº of .hsol files: ",len(hsol_files))
+            
+        # mesh file:
+        mesh_files = [file for file in os.listdir(MESH_PATH) if file.endswith(f".hmesh")] #makes a list of hmesh files
+        
+        if not mesh_files: 
+            raise Exception("No .hmesh found.")
+        else:
+            print(">>> Nº of .hmesh files: ",len(mesh_files))       
